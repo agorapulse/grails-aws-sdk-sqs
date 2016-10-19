3ee286032e48340e97b78105ce5e090d64797a9a
Benoit Hediard <ben@agorapulse.com>
2016-10-19 20:59:46 +0200

    README update [skip ci]

--- travis-publish.sh
+++ travis-publish.sh
@@ -8,7 +8,8 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   if [[ -n $TRAVIS_TAG ]]; then
     echo "Pushing build to Bintray"
     ./gradlew bintrayUpload || EXIT_STATUS=$?
-  #else
+  else
+    echo "Ignoring publishing, no tags found"
     #echo "Publishing snapshot"
     #./gradlew publish || EXIT_STATUS=$?
   fi
